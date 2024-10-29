912c3f5a4d7d21c7a62995cc18f086d5b180cadb
Stephen <stvlley.amzn@gmail.com>
2024-10-29 10:01:16 -0400

    strip away frontend compontents for future features

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -38,19 +38,20 @@ const Navbar = () => {
         </button>
 
         {/* SEARCH BAR  */}
-        <div className="relative">
+        {/* <div className="relative">
           <input
             type="search"
             placeholder="Search your lists"
             className="pl-10 pr-4 py-2 w-50 md:w-60 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500"
           />
 
-          <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-non">
+          <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-non"> */}
 
             {/* SEARCH ICON */}
-            <Search className="text-gray-500" size={20} />
+            {/* <Search className="text-gray-500" size={20} />
           </div>
-        </div>
+        </div> */}
+        
       </div>
 
       {/* RIGHT SIDE */}
@@ -69,16 +70,16 @@ const Navbar = () => {
           </div>
 
           {/* NOTIFICATION ICON */}
-          <div className="relative">
-            <Bell className="cursor-pointer text-gray-500" size={24} />
+           <div className="relative">
+            {/*<Bell className="cursor-pointer text-gray-500" size={24} />
             <span className="absolute -top-2 -right-2 inline-flex items-center justify-center px-[0.4rem] py-1 text-xs font-semibold leading-none text-red-100 bg-red-400 rounded-full">
 
-              {/* NOTIFICATION COUNT WILL BE DISPLAYED HERE */}
+              NOTIFICATION COUNT WILL BE DISPLAYED HERE
               3
             </span>
           </div>
           <hr className="w-0 h-7 border border-solid border-l border-gray-300 mx-3" />
-          <div className="flex items-center gap-3 cursor-pointer">
+          <div className="flex items-center gap-3 cursor-pointer"> */}
 
             {/* PROFILE PICTURE */}
             <Image
@@ -93,9 +94,9 @@ const Navbar = () => {
             <span className="font-semibold">stvlley</span>
           </div>
         </div>
-        <Link href="/settings">
+        {/* <Link href="/settings">
           <Settings className="cursor-pointer text-gray-500" size={24} />
-        </Link>
+        </Link> */}
       </div>
     </div>
   );
