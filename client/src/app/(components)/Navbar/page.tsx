aecb8be6a9601e8e0fddf48699bafd13fb3dad68
Stephen <stvlley.amzn@gmail.com>
2024-11-09 18:46:39 -0500

    clerk

--- client/src/app/(components)/Navbar/page.tsx
+++ client/src/app/(components)/Navbar/page.tsx
@@ -2,6 +2,7 @@
 
 import { useAppDispatch, useAppSelector } from "@/app/redux";
 import { setIsDarkMode, setIsSidebarCollapsed } from "@/state";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { Bell, Menu, Moon, Search, Settings, Sun } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
@@ -30,28 +31,16 @@ const Navbar = () => {
       <div className="flex justify-between items-center gap-5">
 
         {/* TOGGLE SIDEBAR  */}
-        <button
-          className="px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
-          onClick={toggleSidebar}
-        >
-          <Menu className="w-4 h-4" />
-        </button>
+        <SignedIn>
+          <button
+            className="px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
+            onClick={toggleSidebar}
+          >
+            <Menu className="w-4 h-4" />
+          </button>
+        </SignedIn>
 
-        {/* SEARCH BAR  */}
-        {/* <div className="relative">
-          <input
-            type="search"
-            placeholder="Search your lists"
-            className="pl-10 pr-4 py-2 w-50 md:w-60 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500"
-          />
 
-          <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-non"> */}
-
-            {/* SEARCH ICON */}
-            {/* <Search className="text-gray-500" size={20} />
-          </div>
-        </div> */}
-        
       </div>
 
       {/* RIGHT SIDE */}
@@ -68,9 +57,16 @@ const Navbar = () => {
               )}
             </button>
           </div>
-
+          
+          <SignedOut>
+            <Link href="/sign-in">
+              <button className="px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100">
+                Sign In
+              </button>
+            </Link>
+          </SignedOut>
           {/* NOTIFICATION ICON */}
-           <div className="relative">
+          <div className="relative">
             {/*<Bell className="cursor-pointer text-gray-500" size={24} />
             <span className="absolute -top-2 -right-2 inline-flex items-center justify-center px-[0.4rem] py-1 text-xs font-semibold leading-none text-red-100 bg-red-400 rounded-full">
 
@@ -81,17 +77,7 @@ const Navbar = () => {
           <hr className="w-0 h-7 border border-solid border-l border-gray-300 mx-3" />
           <div className="flex items-center gap-3 cursor-pointer"> */}
 
-            {/* PROFILE PICTURE */}
-            <Image
-              src="/favicon.ico" 
-              alt="Profile"
-              width={30}
-              height={30}
-              className="rounded-full h-full object-cover"
-            />
-
-            {/* PROFILE NAME */}
-            <span className="font-semibold">stvlley</span>
+            <UserButton />
           </div>
         </div>
         {/* <Link href="/settings">
